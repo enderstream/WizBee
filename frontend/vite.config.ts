1288070ff73536f94967a58ef758e2721b009cbe
enderstream00 <enderstream00@naver.com>
2025-03-19 04:44:45 +0000

    ci/cd: frontend 배포 완료

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,66 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import { fileURLToPath } from 'node:url'
+
+// // https://vite.dev/config/
+// export default defineConfig((env) => {
+//   const isDevMode = env.mode.includes('development')
+
+//   return {
+//     plugins: [react()],
+//     css: {
+//       devSourcemap: true,
+//     },
+//     resolve: {
+//       alias: {
+//         '@': fileURLToPath(new URL('./src', import.meta.url)),
+//       },
+//     },
+//     server: {
+//       watch: {
+//         usePolling: true
+//       },
+//       host: true,
+//       strictPort: true,
+//       port: 15173,
+//       cors: true,
+//       allowedHosts: ['j12b102.p.ssafy.io', 'localhost']
+//     },
+//     build: {
+//       rollupOptions: {
+//         output: {
+//           manualChunks: {
+//             react: [
+//               'react',
+//               'react-dom',
+//               'react-router',
+//               'react-calendar',
+//               'react-icons',
+//               'react-error-boundary',
+//             ],
+//             ecosystem: ['swiper', 'zustand', 'dayjs'],
+//             mui: ['@mui/material', '@mui/x-date-pickers'],
+//             supabase: ['@supabase/supabase-js'],
+//           },
+//         },
+//       },
+//     },
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { fileURLToPath } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig((env) => {
-  const isDevMode = env.mode.includes('development')
+  const isDevMode = env.mode === 'development'
 
   return {
     plugins: [react()],
     css: {
-      devSourcemap: true,
+      devSourcemap: isDevMode,
     },
     resolve: {
       alias: {
@@ -27,23 +78,11 @@ export default defineConfig((env) => {
       allowedHosts: ['j12b102.p.ssafy.io', 'localhost']
     },
     build: {
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            react: [
-              'react',
-              'react-dom',
-              'react-router',
-              'react-calendar',
-              'react-icons',
-              'react-error-boundary',
-            ],
-            ecosystem: ['swiper', 'zustand', 'dayjs'],
-            mui: ['@mui/material', '@mui/x-date-pickers'],
-            supabase: ['@supabase/supabase-js'],
-          },
-        },
-      },
+      // 개발모드일 때와 프로덕션 모드일 때 다른 설정 적용
+      minify: !isDevMode,
+      sourcemap: isDevMode,
+      // manualChunks 설정 제거
+      outDir: 'dist',
     },
   }
 })
