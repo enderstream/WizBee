b37c5c900a92a64c790cd316b41271b55e893685
WooYeonJu <woojja07@naver.com>
2025-03-23 12:24:03 +0900

    feat: 유저 개인 정보 수정 기능 추가 S12P21B102-37

--- backend/src/main/java/com/wizbee/backend/user/repository/UserRepository.java
+++ backend/src/main/java/com/wizbee/backend/user/repository/UserRepository.java
@@ -7,5 +7,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByMachine(String rassId);
+
+    User findById(int id);
+
 }
 
