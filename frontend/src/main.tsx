703aa0a4b698cae5b7f66059f1d60ed4989f617a
enderstream00 <enderstream00@naver.com>
2025-03-24 17:26:34 +0900

    feat: App과 main 재구성

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/routes'
 import './index.css'
 
+// DOM에 React 앱 마운트하기
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode> {/* 개발 모드에서 문제를 찾기 위한 엄격 모드 활성화 */}
+    <RouterProvider router={router} /> {/* 정의된 라우터 구성을 앱에 제공 */}
+  </React.StrictMode>
 )
