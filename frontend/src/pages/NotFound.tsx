0729781b793b75fa7de07ab50f4f87b39d701dc6
enderstream00 <enderstream00@naver.com>
2025-03-24 12:47:26 +0900

    feat: 라우트 파일로 라우팅 따로 모아두는 중

---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+
+// function NotFound(): JSX.Element{
+//     return(
+//         console.log(`404 NOT FOUND`)
+//     )
+// }
+
+// export default NotFound;
