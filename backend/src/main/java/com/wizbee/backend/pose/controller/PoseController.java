14588ad2338d9deb5c1bfe751d8300da94731c38
조기흠 <whrlgma98@gmail.com>
2025-03-27 12:23:45 +0900

    Feat: 자세 정보 저장

--- backend/src/main/java/com/wizbee/backend/pose/controller/PoseController.java
+++ backend/src/main/java/com/wizbee/backend/pose/controller/PoseController.java
@@ -1,10 +1,31 @@
 package com.wizbee.backend.pose.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.wizbee.backend.pose.dto.PoseStatisticsSaveRequestDto;
+import com.wizbee.backend.pose.entity.Pose;
+import com.wizbee.backend.pose.service.PoseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/vi/pose")
+@RequestMapping("/api/v1/pose")
 public class PoseController {
+
+    private final PoseService poseService;
+
+    public PoseController(PoseService poseService) {
+        this.poseService = poseService;
+    }
+
+    // 자세 통계 저장
+    @PostMapping("/score/{userId}")
+    public ResponseEntity<?> savePose(@PathVariable("userId") int userId, @RequestBody PoseStatisticsSaveRequestDto poseSaveRequestDto) {
+        try {
+            Pose pose = poseService.savePoseStatistics(poseSaveRequestDto, userId);
+            return ResponseEntity.status(HttpStatus.CREATED).body("저장완료");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+        }
+    }
 
 }
--- backend/src/main/java/com/wizbee/backend/pose/service/PoseService.java
+++ backend/src/main/java/com/wizbee/backend/pose/service/PoseService.java
@@ -1,5 +1,39 @@
 package com.wizbee.backend.pose.service;
 
+import com.wizbee.backend.pose.dto.PoseStatisticsSaveRequestDto;
+import com.wizbee.backend.pose.entity.Pose;
+import com.wizbee.backend.pose.repository.PoseRepository;
+import com.wizbee.backend.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PoseService {
+    private final PoseRepository poseRepository;
+    private final UserRepository userRepository;
+
+
+    public PoseService(PoseRepository poseRepository, UserRepository userRepository)
+    {
+        this.poseRepository = poseRepository;
+        this.userRepository = userRepository;
+    }
+
+    // 자세 통계 저장
+    public Pose savePoseStatistics(PoseStatisticsSaveRequestDto poseStatisticsSaveRequestDto, int userId) {
+
+        // DTO -> Entity
+        Pose pose = new Pose();
+        pose.setUser(userRepository.findById(userId));
+        pose.setTutleCnt(poseStatisticsSaveRequestDto.getPoseTurtleCnt());
+        pose.setShoulderCnt(poseStatisticsSaveRequestDto.getShoulderCnt());
+        pose.setDownCnt(poseStatisticsSaveRequestDto.getDownCnt());
+        pose.setDate(poseStatisticsSaveRequestDto.getPoseDate());
+
+        return poseRepository.save(pose);
+
+
+    }
+
+
 
 }
