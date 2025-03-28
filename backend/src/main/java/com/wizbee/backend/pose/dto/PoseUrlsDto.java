6277af71e2f457c2a88c71c2fc6f2bba4bd35394
조기흠 <whrlgma98@gmail.com>
2025-03-28 16:14:15 +0900

    Feat: 잘못된 자세 이미지 조회 S12P21B102-84

---
+++ backend/src/main/java/com/wizbee/backend/pose/dto/PoseUrlsDto.java
@@ -0,0 +1,13 @@
+package com.wizbee.backend.pose.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PoseUrlsDto {
+    private Integer poseImageId;
+    private String poseImageUrl;
+}
--- backend/src/main/java/com/wizbee/backend/pose/repository/PoseImageRepository.java
+++ backend/src/main/java/com/wizbee/backend/pose/repository/PoseImageRepository.java
@@ -1,7 +1,20 @@
 package com.wizbee.backend.pose.repository;
 
+import com.wizbee.backend.pose.dto.PoseUrlsDto;
 import com.wizbee.backend.pose.entity.PoseImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+import java.util.List;
+
+@Repository
 public interface PoseImageRepository extends JpaRepository<PoseImage, Integer> {
+    @Query("SELECT new com.wizbee.backend.pose.dto.PoseUrlsDto(pi.id, pi.imageUrl) "+
+            "FROM PoseImage pi " +
+            "WHERE pi.user.id = :userId " +
+            "AND pi.pose.date = :poseDate")
+    List<PoseUrlsDto> findPoseImageUrls(@Param("userId") int userId, @Param("poseDate") Date poseDate);
 }
