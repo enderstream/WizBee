8ca60539a28c38241aa7861403425a8bc0ad9cf3
조기흠 <whrlgma98@gmail.com>
2025-03-31 17:13:06 +0900

    Feat: 타임랩스 촬영종료 S12P21B102-97

---
+++ backend/src/main/java/com/wizbee/backend/timelapse/dto/TimeLapseFinishRequestDto.java
@@ -0,0 +1,10 @@
+package com.wizbee.backend.timelapse.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TimeLapseFinishRequestDto {
+    private String title;
+}
