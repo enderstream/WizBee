cf694103c744f0001606d48e5191f91d03636f08
enderstream00 <enderstream00@naver.com>
2025-04-10 09:34:30 +0900

    chore: 불필요한 로그, 파일 삭제

--- frontend/src/types/ProfileForm.ts
+++
@@ -1,7 +0,0 @@
-export interface ProfileFormProps {
-    name: string;
-    birthDate: Date | null;
-    setName: (value: string) => void;
-    setBirthDate: (date: Date | null) => void;
-    isLoading?: boolean;
-}
